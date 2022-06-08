938daa38fd26fc0736fe1cac74b1adbc263419ee
Adriano Ribeiro <adrianoribeiro589@gmail.com>
2022-06-07 23:44:41 -0300

    secondCommit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fraldas = new Map([
+    /*const fraldas = new Map([
     ["pequena",11],
     ["media",25],
     ["grande",30]
@@ -8,3 +8,29 @@ const bebida = new Map([
 	["cerveja",0],
 	["refrigerante",0]	
 ]);
+
+const convidados = (nome)=>{
+    const lista = [nome, fraldas , bebida]    
+    
+}
+
+if (document.getElementsByClassName("fralda") = document.getElementsByID("p")){
+
+}*/
+
+
+let fraldaPequena = 11
+let fraldaMedia = 25
+let fraldaGrande = 30
+let cerveja = 0
+let refrigerante = 0
+
+if (document.getElementsByClassName("fralda") = document.getElementsByID("p")){
+fraldaPequena = fraldaPequena - 1}
+    if (fraldaPequena = 0){
+        document.getElementById("p").disabled = true;
+    }
+
+
+
+console.log(fraldaPequena)
